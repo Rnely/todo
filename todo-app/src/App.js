f14d7c60d3eb0069f45648e4fbcae779dc0caaa0
ricards <ricards.sinkjavics@accenture.com>
2023-01-30 17:26:51 +0200

    Made a basic navbar

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,21 +1,13 @@
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+    </Router>
   );
 }
 
---
+++ todo-app/src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>ToDo List</h1>
+            <div className="links">
+
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
