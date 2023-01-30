c4dfb258573b24f699b3f6c21fa0a34806c1ba83
ricards <ricards.sinkjavics@accenture.com>
2023-01-30 17:31:26 +0200

    Added links to navbar and font

--- todo-app/src/Navbar.js
+++ todo-app/src/Navbar.js
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>ToDo List</h1>
             <div className="links">
-
+                <Link to="/">Home</Link>
+                <Link to="/create">New Task</Link>
             </div>
         </nav>
     );
