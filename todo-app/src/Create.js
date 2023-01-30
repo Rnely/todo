515f3784eda1c8c3c059aa726053333540f6378c
ricards <ricards.sinkjavics@accenture.com>
2023-01-30 18:41:19 +0200

    Created page for creating new task and added style to it

---
+++ todo-app/src/Create.js
@@ -0,0 +1,39 @@
+import LocalStorage from "./LocalStorage"
+
+
+const Create = () => {
+    const [name, setName] = LocalStorage('name', '')
+    const [desc, setDesc] = LocalStorage('desc', '')
+    const [date, setDate] = LocalStorage('date', '')
+
+    return (
+        <div className="create">
+            <h2>Add a New Task</h2>
+            <form>
+                <label>Task Name:</label>
+                <input 
+                    type="text"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <label>Task Description:</label>
+                <textarea 
+                    required
+                    value={desc}
+                    onChange={(e) => setDesc(e.target.value)}
+                />
+                <label>Date:</label>
+                <input 
+                    type="date"
+                    required
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                />
+            </form>
+        </div>
+    )
+
+}
+
+export default Create;
